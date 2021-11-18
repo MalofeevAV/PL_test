4984d9cd24cb51d966eb670a67453dd1f4ced6eb
Anton M <9062373@gmail.com>
2021-11-18 20:30:31 +0300

    task1 is done

---
+++ task1/task1.py
@@ -0,0 +1,18 @@
+import sys
+
+n, m = map(int, sys.argv[1:])
+
+
+def path_through_array(n, m):
+    arr = [el for el in range(1, n + 1)]
+    start, end = 0, m - 1
+    answer = arr[:1]
+
+    while arr[0] != arr[end % n]:
+        start, end = end, (end + (m - 1)) % n
+        answer.append(arr[start])
+    return answer
+
+
+if __name__ == "__main__":
+    print("".join(map(str, path_through_array(n, m))), sep="\n")
